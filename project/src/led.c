8aacdcd477de1eb0cf12646e1b81aa1608005582
cchinolla <cmchinolla@miners.utep.edu>
2020-11-01 20:32:01 -0700

    src file

---
+++ project/src/led.c
@@ -0,0 +1,15 @@
+#include <msp430.h>
+#include "led.h"
+
+unsigned char red_on = 0, green_on = 0;
+unsigned char led_changed = 0;
+
+static char redVal[] = {0, LEED_RED}, greenVal[] = {0,LED_GREEN};
+
+void led_init(){
+
+}
+
+void led_update(){
+
+}
