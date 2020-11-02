9169e4d8ee1c79a9e1de0aa715f5e98e410c5196
cchinolla <cmchinolla@miners.utep.edu>
2020-11-01 22:05:04 -0700

    created switch case for count

---
+++ project/src/StateMachine.c
@@ -0,0 +1,22 @@
+#include <msp430.h>
+#include "StateMachine.h"
+#include "led.h"
+#include "buzzer.h"
+
+static char count = 0;
+void countToThree(){
+  switch(count){
+  }
+}
+
+void dimLights(){
+
+}
+
+void resetLEDs(){
+
+}
+
+void resetCount(){
+
+}
